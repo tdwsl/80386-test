d2fecf40aa47fa46a1685524cf66057e6a68559e
tdwsl <tdwstegerlewis@outlook.com>
2022-07-27 18:32:58 +0100

    added printnum

--- printnum.asm
+++ printnum.asm
@@ -1,37 +1,118 @@
-; print numbers
+; print numbers in assembly
 
 section .text
 global _start
 
 _start:
-	mov eax,127
-	call printH
-	mov al,10
-	call printC
+	mov eax,printHex
+	call printOpOnArr
+	mov eax,printInt
+	call printOpOnArr
+	mov eax,printSigned
+	call printOpOnArr
 
 	mov eax,1
 	int 80h
 
-printH:
+printOpOnArr:
+	mov ecx,[numNumbers]
+	mov edx,numbers
+
+printOpOnArrL:
+	pusha
+	mov ebx,eax
+	mov eax,[edx]
+	call ebx
+	popa
+
+	cmp ecx,1
+	jz printOpOnArrNoC
+
+	pusha
+	mov al,','
+	call printChar
+	mov al,' '
+	call printChar
+	popa
+printOpOnArrNoC:
+
+	add edx,4
+	loop printOpOnArrL
+
+	mov al,10
+	call printChar
+	ret
+
+printSigned:
+	mov ebx,eax
+	and ebx,80000000h
+	jz printSignedPos
+	neg eax
+	push eax
+	mov al,'-'
+	call printChar
+	pop eax
+printSignedPos:
+	call printInt
+	ret
+
+printInt:
+
+	or eax,eax
+	jnz printIntNZ
+	mov al,'0'
+	call printChar
+	ret
+printIntNZ:
+
+	mov edi,work
+	mov ebx,10
+printIntL:
+	mov edx,0
+	div ebx
+	push eax
+	mov eax,edx
+	add al,'0'
+	stosb
+	pop eax
+	cmp eax,edx
+	jz printIntFin
+	jmp printIntL
+
+printIntFin:
+	dec edi
+	dec edi
+	mov esi,edi
+	std
+
+printIntPL:
+	lodsb
+	call printChar
+	cmp esi,work
+	jnb printIntPL
+	cld
+	ret
+
+printHex:
 	push eax
 	mov al,'0'
-	call printC
+	call printChar
 	mov al,'x'
-	call printC
+	call printChar
 	pop eax
 
 	mov ecx,8
-printHL:
+printHexL:
 	push ecx
 	push eax
 	and al,0fh
 	cmp al,10
-	jb printHB10
+	jb printHexB10
 	add al,'a'-10
-	jmp printHA10
-printHB10:
+	jmp printHexA10
+printHexB10:
 	add al,'0'
-printHA10:
+printHexA10:
 
 	mov ecx,[esp+4]
 	add ecx,work
@@ -41,7 +122,7 @@ printHA10:
 	pop eax
 	pop ecx
 	shr eax,4
-	loop printHL
+	loop printHexL
 
 	mov eax,4
 	mov ebx,1
@@ -51,7 +132,7 @@ printHA10:
 
 	ret
 
-printC:
+printChar:
 	mov [cbuf],al
 	mov eax,4
 	mov ebx,1
@@ -59,6 +140,13 @@ printC:
 	mov edx,1
 	int 80h
 	ret
+
+section .data
+
+numbers:
+	dd 0,12,-357,2001,-3,0ffffffffh
+numNumbers:
+	dd 6
 
 section .bss
 
